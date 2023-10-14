ca3856a19a622c4ab4b45b260098e6cb2abcf89d
Eliud Gateri <egateri@gmail.com>
2023-10-14 22:23:26 +0300

    Update Database Connection

--- src/main/java/com/moringaschool/student/Main.java
+++ src/main/java/com/moringaschool/student/Main.java
@@ -1,8 +1,60 @@
 package com.moringaschool.student;
 
-public class Main {
-    public static void main(String[] args) {
+import com.moringaschool.student.config.DatabaseConfig;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
-        System.out.println("Hello world!");
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+         Connection connection = DatabaseConfig.getConnection();
+//        Sql2o sql2o = DatabaseConfig.getDatabase();
+
+       get("/hello",(request, response)->{
+           HandlebarsTemplateEngine template = new HandlebarsTemplateEngine();
+           Map<String, Object> payload = new HashMap<>();
+           ModelAndView model = new ModelAndView(template,"mash");
+
+            return "Connection is OK!";
+
+
+
+//  return "Hello World!";
+
+//           try(Connection connection = sql2o.open()){
+//
+//
+//               String msg = connection.toString();
+//
+//               System.out.println("SUCCESS "+msg);
+//
+//               return msg;
+//
+//           }
+//           catch (Exception exception){
+//
+//               System.out.println("ERROR "+exception.getLocalizedMessage());
+//
+//               return exception.getLocalizedMessage();
+//
+//           }
+
+       });
+
+       get("/",(request,response)->{
+           return "Home";
+       });
+
+
+
+
     }
 }
--- src/main/java/com/moringaschool/student/config/DatabaseConfig.java
+++ src/main/java/com/moringaschool/student/config/DatabaseConfig.java
@@ -1,9 +1,19 @@
 package com.moringaschool.student.config;
 
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class DatabaseConfig {
-    public static Sql2o getDatabase(){
-        return new Sql2o("jdbc:postgresql://localhost:5432/herosquad","postgres","");
+    public static Connection getConnection(){
+        Sql2o sql20 = new Sql2o("jdbc:postgresql://localhost:5432/herosquad","postgres1","postgres");
+        try(Connection connection= sql20.open()){
+            System.out.println("SUCCESS "+connection.toString());
+            return connection;
+        }
+        catch(Exception exception){
+            System.out.println("ERROR "+exception.getLocalizedMessage());
+            return null;
+        }
     }
+
 }
