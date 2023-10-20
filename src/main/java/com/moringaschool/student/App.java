4b07ea1880e2db08eb46f236bd02802fa6a68d81
Eliud Gateri <egateri@gmail.com>
2023-10-20 21:24:49 +0300

    Update Project Structure

--- src/main/java/com/moringaschool/student/App.java
+++ src/main/java/com/moringaschool/student/App.java
@@ -2,16 +2,17 @@ package com.moringaschool.student;
 
 import com.moringaschool.student.config.DatabaseConfig;
 import org.sql2o.Connection;
-import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
-public class Main {
+public class App {
 
 
     public static void main(String[] args) {
@@ -21,31 +22,15 @@ public class Main {
        get("/hello",(request, response)->{
            HandlebarsTemplateEngine template = new HandlebarsTemplateEngine();
            Map<String, Object> payload = new HashMap<>();
-           ModelAndView model = new ModelAndView(template,"home.hbs");
+           ModelAndView model = new ModelAndView(payload,"home.hbs");
 
-            return "Connection is OK!";
-
+//            return "Connection is OK!";
+return template.render(model);
 
 
 //  return "Hello World!";
 
-//           try(Connection connection = sql2o.open()){
-//
-//
-//               String msg = connection.toString();
-//
-//               System.out.println("SUCCESS "+msg);
-//
-//               return msg;
-//
-//           }
-//           catch (Exception exception){
-//
-//               System.out.println("ERROR "+exception.getLocalizedMessage());
-//
-//               return exception.getLocalizedMessage();
-//
-//           }
+
 
        });
 
@@ -65,8 +50,24 @@ public class Main {
             );
 
         get("/home",(request, response)->{
+            if(connection!=null){
 
-            return new ModelAndView(new HashMap<>(),"home.hbs");
+                Map<String, Object> payload =new HashMap<>();
+                List<String> list = new ArrayList<>();
+                list.add("Eliud");
+                list.add("Ann");
+                list.add("Andrew");
+                list.add("Ken");
+                list.add("Kennedy");
+                list.add("Peris");
+                payload.put("user",list);
+                return new ModelAndView(payload,"home.hbs");
+            }
+            else{
+
+                return new ModelAndView(new HashMap<>(),"error.hbs");
+            }
+
 
                 },new HandlebarsTemplateEngine());
 
--- src/main/java/com/moringaschool/student/config/DatabaseConfig.java
+++ src/main/java/com/moringaschool/student/config/DatabaseConfig.java
@@ -3,14 +3,17 @@ package com.moringaschool.student.config;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.sql.SQLException;
+
 public class DatabaseConfig {
     public static Connection getConnection(){
-        Sql2o sql20 = new Sql2o("jdbc:postgresql://localhost:5432/herosquad","postgres","postgres");
-        try(Connection connection= sql20.open()){
+        Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/springboot","postgres","postgres");
+        try(Connection connection= sql2o.open()){
             System.out.println("SUCCESS "+connection.toString());
             return connection;
         }
         catch(Exception exception){
+
             System.out.println("ERROR "+exception.getLocalizedMessage());
             return null;
         }
