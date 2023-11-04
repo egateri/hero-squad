52fbf5e9b79ccfa0e579604ac6d880019ae1e5d3
Eliud Gateri <egateri@gmail.com>
2023-11-04 19:23:52 +0300

    Add YAML File

--- src/main/java/com/moringaschool/student/config/DBConfig.java
+++ src/main/java/com/moringaschool/student/config/DBConfig.java
@@ -5,7 +5,7 @@ import org.sql2o.Sql2o;
 
 public class DBConfig {
     public static Connection getConnection(){
-        Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/herosquad","postgres","postgres");
+        Sql2o sql2o = new Sql2o("jdbc:postgresql://postgresdb:5432/herosquad","postgres","postgres");
         try(Connection connection= sql2o.open()){
             System.out.println("SUCCESS "+connection.toString());
             return connection;
