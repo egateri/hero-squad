bd17bac66d391d0bb91b9631277ea933b2fa74a2
Eliud Gateri <egateri@gmail.com>
2023-11-05 04:04:03 +0300

    Update Database Environment VAriables

--- src/main/java/com/moringaschool/student/config/DBConfig.java
+++ src/main/java/com/moringaschool/student/config/DBConfig.java
@@ -3,9 +3,23 @@ package com.moringaschool.student.config;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.Map;
+
 public class DBConfig {
     public static Connection getConnection(){
-        Sql2o sql2o = new Sql2o("jdbc:postgresql://postgresdb:5432/herosquad","postgres","postgres");
+
+        Map<String, String> env = System.getenv();
+        String DB_HOST =env.get("DB_HOST");
+        String DB_USER =env.get("DB_USER");
+        String DB_DATABASE =env.get("DB_USER");
+        String DB_PASSWORD =env.get("DB_PASSWORD");
+        String DB_PORT =env.get("PORT");
+                for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName,
+                    env.get(envName));
+        }
+        Sql2o sql2o = new Sql2o("jdbc:postgresql://"+DB_HOST+":"+DB_PORT+"/"+DB_DATABASE,DB_USER,DB_PASSWORD);
+
         try(Connection connection= sql2o.open()){
             System.out.println("SUCCESS "+connection.toString());
             return connection;
