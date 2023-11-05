90ef7e7ae001fcd853b4fee587b9b292d6baf916
Eliud Gateri <egateri@gmail.com>
2023-11-05 11:34:32 +0300

    Update DB connection Test Case

--- src/test/java/com/moringa/student/config/DBConfigTest.java
+++ src/test/java/com/moringa/student/config/DBConfigTest.java
@@ -1,19 +1,13 @@
-//package com.moringa.student.config;
-//
-//import com.google.common.annotations.VisibleForTesting;
-//import com.moringaschool.student.config.DBConfig;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.sql2o.Connection;
-//
-//import java.util.Collection;
-//
-//public class DBConfigTest {
-//
-//
-//    @Test
-//    public void test_database_connection(){
-//        Connection connection = DBConfig.getConnection();
-//
-//    }
-//}
+package com.moringa.student.config;
+import com.moringaschool.student.config.DBConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+
+
+public class DBConfigTest {
+    @Test
+    public void test_database_connection(){
+        Connection connection = DBConfig.getConnection();
+    }
+}
